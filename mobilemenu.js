ac31d12a298d222d99a620e2f3b05614eef5a6b1
Sulav Adhikari <141142171+sulav063@users.noreply.github.com>
2025-02-25 19:04:51 +0545

    Update mobilemenu.js

--- mobilemenu.js
+++ mobilemenu.js
@@ -1,19 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
     const menuToggle = document.createElement("button");
-    menuToggle.innerText = "☰";
+    menuToggle.innerHTML = "&#9776;"; // ☰ icon
     menuToggle.classList.add("menu-toggle");
     document.querySelector(".header").appendChild(menuToggle);
 
     const navbar = document.querySelector(".navbar");
+
     menuToggle.addEventListener("click", () => {
         navbar.classList.toggle("open");
+        if (navbar.classList.contains("open")) {
+            menuToggle.innerHTML = "&times;"; // Change to ✖ when menu is open
+        } else {
+            menuToggle.innerHTML = "&#9776;"; // Change back to ☰ when menu is closed
+        }
     });
 
-    // Close menu when clicking on a link
+    // Close menu when clicking a link
     const navLinks = document.querySelectorAll(".navbar a");
     navLinks.forEach(link => {
         link.addEventListener("click", () => {
             navbar.classList.remove("open");
+            menuToggle.innerHTML = "&#9776;"; // Reset to ☰ when closing menu
         });
     });
 });
